b4b8bd69e5458af4782be0f0ce9b6a39c8450a2c
Paul Fryer <paulfryer@macrowebservices.com>
2017-09-16 05:02:55 -0700

    ignore file added

--- DotStep.Core/Choice.cs
+++ DotStep.Core/Choice.cs
@@ -4,7 +4,7 @@ namespace DotStep.Core
 {
     public class Choice 
     {
-        public string Variable { get; set; }
+        public string Variable { get; set; } 
         public string Operator { get; set; }
         public object Value { get; set; }
         public Type Next { get; set; }
